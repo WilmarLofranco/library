99521f0b6e3bb4066682a612db0cafabc3f2c9ca
Wilmar <wilmarlofranco@gmail.com>
2024-09-19 09:30:48 +0800

    Create files needed, build html boilerplate and linked js,css

---
+++ script.js
@@ -0,0 +1,15 @@
+// Storage
+const myLibrary = [];
+
+
+// Constructor
+function Book() {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read
+}
+
+function addBookToLibrary() {
+  // do stuff here
+}
